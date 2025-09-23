2eda8c9960fdd5c71bcdeba9350ea701b9d693c7
Henrik Koski <henrikkoski00@gmail.com>
2025-09-23 16:22:52 +0300

    Fix sh script

--- bin/open_designer.sh
+++ bin/open_designer.sh
@@ -5,4 +5,4 @@ echo "Opening QT designer"
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 PROJECT_DIR=$(dirname -- $SCRIPT_DIR)
 
-$PROJECT_DIR/.venv/lib/python3.12/site-packages/PySide6/designer
+$PROJECT_DIR/.venv/lib/python3.12/site-packages/PySide6/designer &
